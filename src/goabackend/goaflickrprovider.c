GNOME/gnome-online-accounts

commit f5119db95aab7e97a30aca646833bcd72d4ba51f

Update to new webkitgtk-2.1.90 API
Fixes: https://bugzilla.gnome.org/707006

src/goabackend/goaflickrprovider.c
@@ -285,8 +285,8 @@ is_deny_node (GoaOAuthProvider *provider, WebKitDOMNode *node)
   if (!WEBKIT_DOM_IS_HTML_ANCHOR_ELEMENT (node))
     goto out;
 
-  element = WEBKIT_DOM_HTML_ELEMENT (node);
-  id = webkit_dom_html_element_get_id (element);
+  element = WEBKIT_DOM_ELEMENT (node);
+  id = webkit_dom_element_get_id (element);
   if (g_strcmp0 (id, "auth-disallow") != 0)
     goto out;
 

src/goabackend/goagoogleprovider.c
@@ -270,8 +270,8 @@ is_deny_node (GoaOAuth2Provider *provider, WebKitDOMNode *node)
   if (!WEBKIT_DOM_IS_HTML_BUTTON_ELEMENT (node))
     goto out;
 
-  element = WEBKIT_DOM_HTML_ELEMENT (node);
-  id = webkit_dom_html_element_get_id (element);
+  element = WEBKIT_DOM_ELEMENT (node);
+  id = webkit_dom_element_get_id (element);
   if (g_strcmp0 (id, "submit_deny_access") != 0)
     goto out;
 
@@ -300,7 +300,7 @@ is_identity_node (GoaOAuth2Provider *provider, WebKitDOMHTMLInputElement *elemen
   if (g_strcmp0 (element_type, "email") != 0)
     goto out;
 
-  id = webkit_dom_html_element_get_id (WEBKIT_DOM_HTML_ELEMENT (element));
+  id = webkit_dom_element_get_id (WEBKIT_DOM_ELEMENT (element));
   if (g_strcmp0 (id, "Email") != 0)
     goto out;
 

configure.ac
@@ -91,7 +91,7 @@ PKG_CHECK_MODULES(GTK, [gtk+-3.0 >= 3.5.1])
 AC_SUBST(GTK_CFLAGS)
 AC_SUBST(GTK_LIBS)
 
-PKG_CHECK_MODULES(WEBKIT_GTK, [webkitgtk-3.0])
+PKG_CHECK_MODULES(WEBKIT_GTK, [webkitgtk-3.0 >= 2.1.90])
 AC_SUBST(WEBKIT_GTK_CFLAGS)
 AC_SUBST(WEBKIT_GTK_LIBS)
 

commit 70b37bde1b1b0e10a941fd46cfd78706fb7f8129

Fix the pointer types
Fallout from f5119db95aab7e97a30aca646833bcd72d4ba51f

Fixes: https://bugzilla.gnome.org/707006

src/goabackend/goaflickrprovider.c
@@ -275,7 +275,7 @@ get_identity_sync (GoaOAuthProvider  *provider,
 static gboolean
 is_deny_node (GoaOAuthProvider *provider, WebKitDOMNode *node)
 {
-  WebKitDOMHTMLElement *element;
+  WebKitDOMElement *element;
   gboolean ret;
   gchar *id;
 

src/goabackend/goagoogleprovider.c
@@ -260,7 +260,7 @@ get_identity_sync (GoaOAuth2Provider  *provider,
 static gboolean
 is_deny_node (GoaOAuth2Provider *provider, WebKitDOMNode *node)
 {
-  WebKitDOMHTMLElement *element;
+  WebKitDOMElement *element;
   gboolean ret;
   gchar *id;
 