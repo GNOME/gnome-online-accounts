4d80e3503b4b0ae4e379f6e14da562087c58d798
Debarshi Ray <debarshir@gnome.org>
2014-04-23 14:53:17 +0200

    live: Don't leak the GoaDocuments
    Fixes: https://bugzilla.gnome.org/728787

--- src/goabackend/goawindowsliveprovider.c
+++ src/goabackend/goawindowsliveprovider.c
@@ -314,6 +314,7 @@ build_object (GoaProvider         *provider,
 
   account = NULL;
   mail = NULL;
+  documents = NULL;
 
   /* Chain up */
   if (!GOA_PROVIDER_CLASS (goa_windows_live_provider_parent_class)->build_object (provider,
@@ -394,6 +395,7 @@ build_object (GoaProvider         *provider,
   ret = TRUE;
 
  out:
+  g_clear_object (&documents);
   g_clear_object (&mail);
   if (account != NULL)
     g_object_unref (account);
