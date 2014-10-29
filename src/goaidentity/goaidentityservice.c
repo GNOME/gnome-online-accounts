161e29493b2cfe2cbb641790ae808de91edcb7ed
Ray Strode <rstrode@redhat.com>
2014-10-29 16:12:31 -0400

    identity: plug leak in identity service
    When the identity service first starts up, it gets a list
    of preexisting kerberos principals and then exports them
    over the bus.
    
    The object path used for the exported objects is getting leaked.
    This commit fixes that.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=739590

--- src/goaidentity/goaidentityservice.c
+++ src/goaidentity/goaidentityservice.c
@@ -1607,8 +1607,9 @@ on_identities_listed (GoaIdentityManager *manager,
       GoaIdentity *identity = node->data;
       const char  *principal;
       GoaObject   *object;
+      char        *object_path;
 
-      export_identity (self, identity);
+      object_path = export_identity (self, identity);
 
       principal = goa_identity_get_identifier (identity);
 
@@ -1618,6 +1619,8 @@ on_identities_listed (GoaIdentityManager *manager,
         add_temporary_account (self, identity);
       else
         g_object_unref (object);
+
+      g_free (object_path);
     }
 
  out:
