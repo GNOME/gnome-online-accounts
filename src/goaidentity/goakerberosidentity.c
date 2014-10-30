67a590e6661b2ae3ad6621457f2ca7c2c9cf130b
Ray Strode <rstrode@redhat.com>
2014-10-30 14:12:08 -0400

    identity: Destroy alarms from main thread, even on dispose
    These days we defer destroying alarms to the main thread when
    alarms are reset.  We still dispose of alarms in the current
    thread when disposing their identity object, however.
    
    This commit changes the code to always dispose of alarms from
    the main thread.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=739593

--- src/goaidentity/goakerberosidentity.c
+++ src/goaidentity/goakerberosidentity.c
@@ -105,9 +105,7 @@ goa_kerberos_identity_dispose (GObject *object)
   GoaKerberosIdentity *self = GOA_KERBEROS_IDENTITY (object);
 
   G_LOCK (identity_lock);
-  g_clear_object (&self->priv->renewal_alarm);
-  g_clear_object (&self->priv->expiring_alarm);
-  g_clear_object (&self->priv->expiration_alarm);
+  clear_alarms (self);
   G_UNLOCK (identity_lock);
 
   G_OBJECT_CLASS (goa_kerberos_identity_parent_class)->dispose (object);
