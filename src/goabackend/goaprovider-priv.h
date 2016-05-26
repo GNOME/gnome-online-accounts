e2f64a91e45cdbdeb0651231f7ccd513903237cf
Debarshi Ray <debarshir@gnome.org>
2016-05-26 19:08:41 +0200

    provider: Add a new vfunc for backend-specific initialization
    https://bugzilla.gnome.org/show_bug.cgi?id=766733

--- src/goabackend/goaprovider-priv.h
+++ src/goabackend/goaprovider-priv.h
@@ -52,6 +52,7 @@ struct _GoaProvider
  * @ensure_credentials_sync: Virtual function for goa_provider_ensure_credentials_sync().
  * @show_account: Virtual function for goa_provider_show_account().
  * @get_credentials_generation: Virtual function for goa_provider_get_credentials_generation().
+ * @initialize: Virtual function for goa_provider_initialize().
  *
  * Class structure for #GoaProvider.
  */
@@ -92,6 +93,7 @@ struct _GoaProviderClass
   guint                   (*get_credentials_generation)   (GoaProvider            *self);
   GIcon                  *(*get_provider_icon)            (GoaProvider            *self,
                                                            GoaObject              *object);
+  void                    (*initialize)                   (GoaProvider            *self);
   void                    (*remove_account)               (GoaProvider            *self,
                                                            GoaObject              *object,
                                                            GCancellable           *cancellable,
@@ -125,6 +127,8 @@ struct _GoaProviderClass
 void        goa_provider_ensure_builtins_loaded                (void);
 
 void        goa_provider_ensure_extension_points_registered    (void);
+
+void        goa_provider_initialize                            (GoaProvider             *self);
 
 void        goa_provider_remove_account                        (GoaProvider             *self,
                                                                 GoaObject               *object,
