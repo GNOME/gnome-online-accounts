GNOME/gnome-online-accounts

commit c88ba443660000d1a755caa4114555d0d2366367

owncloud: Re-title it as Nextcloud
https://bugzilla.gnome.org/show_bug.cgi?id=768515

src/goabackend/goaowncloudprovider.c
@@ -65,7 +65,7 @@ get_provider_type (GoaProvider *provider)
 static gchar *
 get_provider_name (GoaProvider *provider, GoaObject *object)
 {
-  return g_strdup(_("ownCloud"));
+  return g_strdup(_("Nextcloud"));
 }
 
 static GoaProviderGroup

commit e7d3d21923acc4a515760f0907e783a95ba3fb0e

owncloud: Use "Nextcloud", not "ownCloud", in user-visible errors
Fallout from c88ba443660000d1a755caa4114555d0d2366367

https://gitlab.gnome.org/GNOME/gnome-online-accounts/merge_requests/21

src/goabackend/goaowncloudprovider.c
@@ -728,7 +728,7 @@ add_account (GoaProvider    *provider,
         }
 
       markup = g_strdup_printf ("<b>%s:</b>\n%s",
-                                _("Error connecting to ownCloud server"),
+                                _("Error connecting to Nextcloud server"),
                                 data.error->message);
       g_clear_error (&data.error);
 
@@ -919,7 +919,7 @@ refresh_account (GoaProvider    *provider,
       gchar *markup;
 
       markup = g_strdup_printf ("<b>%s:</b>\n%s",
-                                _("Error connecting to ownCloud server"),
+                                _("Error connecting to Nextcloud server"),
                                 data.error->message);
       g_clear_error (&data.error);
 