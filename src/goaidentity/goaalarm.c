GNOME/gnome-online-accounts

commit a43777eb40efb6dae5128397faa8000e59ba0f04

daemon: Add kerberos renewal service
This commit adds an identity service whose purpose is
to automatically renew expiring kerberos credentials,
and to expose a mechanism over the bus to "kinit".

This service lays the groundwork for integrating a
kerberos provider into gnome-online-accounts.

A subsequent commit will add the provider itself.

Some changes by Debarshi Ray

https://bugzilla.gnome.org/show_bug.cgi?id=679253

src/goaidentity/goaalarm.c
@@ -1,0 +1,611 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/*
+ * Copyright (C) 2012 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Author: Ray Strode
+ * Based on work by Colin Walters
+ */
+
+#include "config.h"
+
+#include "goaalarm.h"
+
+#ifdef HAVE_TIMERFD
+#include <sys/timerfd.h>
+#endif
+
+#include <unistd.h>
+#include <string.h>
+
+#include <glib.h>
+#include <glib/gi18n.h>
+#include <gio/gio.h>
+#include <gio/gunixinputstream.h>
+
+#include "goalogging.h"
+
+typedef struct
+{
+  GSource *source;
+  GInputStream *stream;
+} Timer;
+
+typedef struct
+{
+  GSource *source;
+} Timeout;
+
+#define MAX_TIMEOUT_INTERVAL (10 *1000)
+
+typedef enum
+{
+  GOA_ALARM_TYPE_UNSCHEDULED,
+  GOA_ALARM_TYPE_TIMER,
+  GOA_ALARM_TYPE_TIMEOUT,
+} GoaAlarmType;
+
+struct _GoaAlarmPrivate
+{
+  GCancellable *cancellable;
+  gulong cancelled_id;
+  GDateTime *time;
+  GDateTime *previous_wakeup_time;
+  GMainContext *context;
+  GSource *immediate_wakeup_source;
+  GRecMutex lock;
+
+  GoaAlarmType type;
+  union
+  {
+    Timer timer;
+    Timeout timeout;
+  };
+};
+
+enum
+{
+  FIRED,
+  REARMED,
+  NUMBER_OF_SIGNALS,
+};
+
+enum
+{
+  PROP_0,
+  PROP_TIME
+};
+
+static void schedule_wakeups (GoaAlarm *self);
+static void schedule_wakeups_with_timeout_source (GoaAlarm *self);
+static guint signals[NUMBER_OF_SIGNALS] = { 0 };
+
+G_DEFINE_TYPE (GoaAlarm, goa_alarm, G_TYPE_OBJECT);
+
+static void
+clear_scheduled_immediate_wakeup (GoaAlarm *self)
+{
+  g_clear_pointer (&self->priv->immediate_wakeup_source,
+                   (GDestroyNotify) g_source_destroy);
+}
+
+static void
+clear_scheduled_timer_wakeups (GoaAlarm *self)
+{
+#ifdef HAVE_TIMERFD
+  GError *error;
+  gboolean is_closed;
+
+  g_clear_pointer (&self->priv->timer.source, (GDestroyNotify) g_source_destroy);
+
+  error = NULL;
+  is_closed = g_input_stream_close (self->priv->timer.stream, NULL, &error);
+
+  if (!is_closed)
+    {
+      goa_warning ("GoaAlarm: could not close timer stream: %s", error->message);
+      g_error_free (error);
+    }
+
+  g_clear_object (&self->priv->timer.stream);
+#endif
+}
+
+static void
+clear_scheduled_timeout_wakeups (GoaAlarm *self)
+{
+  g_clear_pointer (&self->priv->timeout.source, (GDestroyNotify) g_source_destroy);
+}
+
+static void
+clear_scheduled_wakeups (GoaAlarm *self)
+{
+  g_rec_mutex_lock (&self->priv->lock);
+  clear_scheduled_immediate_wakeup (self);
+
+  switch (self->priv->type)
+    {
+    case GOA_ALARM_TYPE_TIMER:
+      clear_scheduled_timer_wakeups (self);
+      break;
+
+    case GOA_ALARM_TYPE_TIMEOUT:
+      clear_scheduled_timeout_wakeups (self);
+      break;
+
+    default:
+      break;
+    }
+
+  g_clear_object (&self->priv->cancellable);
+
+  g_clear_pointer (&self->priv->context, (GDestroyNotify) g_main_context_unref);
+
+  g_clear_pointer (&self->priv->previous_wakeup_time,
+                   (GDestroyNotify) g_date_time_unref);
+
+  g_clear_pointer (&self->priv->time, (GDestroyNotify) g_date_time_unref);
+
+  g_assert (self->priv->timeout.source == NULL);
+
+  self->priv->type = GOA_ALARM_TYPE_UNSCHEDULED;
+  g_rec_mutex_unlock (&self->priv->lock);
+}
+
+static void
+goa_alarm_finalize (GObject *object)
+{
+  GoaAlarm *self = GOA_ALARM (object);
+
+  clear_scheduled_wakeups (self);
+
+  G_OBJECT_CLASS (goa_alarm_parent_class)->finalize (object);
+}
+
+static void
+goa_alarm_set_property (GObject      *object,
+                        guint         property_id,
+                        const GValue *value,
+                        GParamSpec   *param_spec)
+{
+  GoaAlarm *self = GOA_ALARM (object);
+  GDateTime *time;
+
+  switch (property_id)
+    {
+    case PROP_TIME:
+      time = (GDateTime *) g_value_get_boxed (value);
+      goa_alarm_set_time (self, time, self->priv->cancellable);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, param_spec);
+      break;
+    }
+}
+
+static void
+goa_alarm_get_property (GObject    *object,
+                        guint       property_id,
+                        GValue     *value,
+                        GParamSpec *param_spec)
+{
+  GoaAlarm *self = GOA_ALARM (object);
+
+  switch (property_id)
+    {
+    case PROP_TIME:
+      g_value_set_boxed (value, self->priv->time);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, param_spec);
+      break;
+    }
+}
+
+static void
+goa_alarm_class_init (GoaAlarmClass *klass)
+{
+  GObjectClass *object_class;
+
+  object_class = G_OBJECT_CLASS (klass);
+
+  object_class->finalize = goa_alarm_finalize;
+  object_class->get_property = goa_alarm_get_property;
+  object_class->set_property = goa_alarm_set_property;
+
+  g_type_class_add_private (klass, sizeof (GoaAlarmPrivate));
+
+  signals[FIRED] = g_signal_new ("fired",
+                                 G_TYPE_FROM_CLASS (klass),
+                                 G_SIGNAL_RUN_LAST,
+                                 0, NULL, NULL, NULL, G_TYPE_NONE, 0);
+
+  signals[REARMED] = g_signal_new ("rearmed",
+                                   G_TYPE_FROM_CLASS (klass),
+                                   G_SIGNAL_RUN_LAST,
+                                   0, NULL, NULL, NULL, G_TYPE_NONE, 0);
+
+  g_object_class_install_property (object_class,
+                                   PROP_TIME,
+                                   g_param_spec_boxed ("time",
+                                                       _("Time"),
+                                                       _("Time to fire"),
+                                                       G_TYPE_DATE_TIME,
+                                                       G_PARAM_READWRITE));
+}
+
+static void
+goa_alarm_init (GoaAlarm *self)
+{
+  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self, GOA_TYPE_ALARM, GoaAlarmPrivate);
+  self->priv->type = GOA_ALARM_TYPE_UNSCHEDULED;
+  g_rec_mutex_init (&self->priv->lock);
+}
+
+static void
+on_cancelled (GCancellable *cancellable, gpointer user_data)
+{
+  GoaAlarm *self = GOA_ALARM (user_data);
+
+  clear_scheduled_wakeups (self);
+}
+
+static void
+fire_alarm (GoaAlarm *self)
+{
+  g_signal_emit (G_OBJECT (self), signals[FIRED], 0);
+}
+
+static void
+rearm_alarm (GoaAlarm *self)
+{
+  g_signal_emit (G_OBJECT (self), signals[REARMED], 0);
+}
+
+static void
+fire_or_rearm_alarm (GoaAlarm *self)
+{
+  GTimeSpan time_until_fire;
+  GTimeSpan previous_time_until_fire;
+  GDateTime *now;
+
+  now = g_date_time_new_now_local ();
+  time_until_fire = g_date_time_difference (self->priv->time, now);
+
+  if (self->priv->previous_wakeup_time == NULL)
+    {
+      self->priv->previous_wakeup_time = now;
+
+      /* If, according to the time, we're past when we should have fired,
+       * then fire the alarm.
+       */
+      if (time_until_fire <= 0)
+        fire_alarm (self);
+    }
+  else
+    {
+      previous_time_until_fire =
+          g_date_time_difference (self->priv->time,
+                                  self->priv->previous_wakeup_time);
+
+      g_date_time_unref (self->priv->previous_wakeup_time);
+      self->priv->previous_wakeup_time = now;
+
+      /* If, according to the time, we're past when we should have fired,
+       * and this is the first wakeup where that's been true then fire
+       * the alarm. The first check makes sure we don't fire prematurely,
+       * and the second check makes sure we don't fire more than once
+       */
+      if (time_until_fire <= 0 && previous_time_until_fire > 0)
+        {
+          fire_alarm (self);
+
+          /* If, according to the time, we're before when we should fire,
+           * and we previously fired the alarm, then we've jumped back in
+           * time and need to rearm the alarm.
+           */
+        }
+      else if (time_until_fire > 0 && previous_time_until_fire <= 0)
+        {
+          rearm_alarm (self);
+        }
+    }
+}
+
+static gboolean
+on_immediate_wakeup_source_ready (GoaAlarm *self)
+{
+  g_return_val_if_fail (self->priv->type != GOA_ALARM_TYPE_UNSCHEDULED, FALSE);
+
+  g_rec_mutex_lock (&self->priv->lock);
+  if (g_cancellable_is_cancelled (self->priv->cancellable))
+    goto out;
+
+  fire_or_rearm_alarm (self);
+
+out:
+  g_rec_mutex_unlock (&self->priv->lock);
+  return FALSE;
+}
+
+#ifdef HAVE_TIMERFD
+static gboolean
+on_timer_source_ready (GObject *stream, GoaAlarm *self)
+{
+  gint64 number_of_fires;
+  gssize bytes_read;
+  gboolean run_again = FALSE;
+
+  g_return_val_if_fail (GOA_IS_ALARM (self), FALSE);
+  g_return_val_if_fail (self->priv->type == GOA_ALARM_TYPE_TIMER, FALSE);
+
+  g_rec_mutex_lock (&self->priv->lock);
+  if (g_cancellable_is_cancelled (self->priv->cancellable))
+    goto out;
+
+  bytes_read =
+    g_pollable_input_stream_read_nonblocking (G_POLLABLE_INPUT_STREAM (stream),
+                                              &number_of_fires, sizeof (gint64),
+                                              NULL, NULL);
+
+  if (bytes_read == sizeof (gint64))
+    {
+      if (number_of_fires < 0 || number_of_fires > 1)
+        {
+          goa_warning ("GoaAlarm: expected timerfd to report firing once,"
+                       "but it reported firing %ld times\n", (long) number_of_fires);
+        }
+    }
+
+  fire_or_rearm_alarm (self);
+  run_again = TRUE;
+out:
+  g_rec_mutex_unlock (&self->priv->lock);
+  return run_again;
+}
+
+static void
+clear_timer_source_pointer (GoaAlarm *self)
+{
+  self->priv->timer.source = NULL;
+}
+#endif
+
+static gboolean
+schedule_wakeups_with_timerfd (GoaAlarm *self)
+{
+#ifdef HAVE_TIMERFD
+  struct itimerspec timer_spec;
+  int fd;
+  int result;
+  GSource *source;
+  static gboolean seen_before = FALSE;
+
+  if (!seen_before)
+    {
+      goa_debug ("GoaAlarm: trying to use kernel timer");
+      seen_before = TRUE;
+    }
+
+  fd = timerfd_create (CLOCK_REALTIME, TFD_CLOEXEC | TFD_NONBLOCK);
+
+  if (fd < 0)
+    {
+      goa_debug ("GoaAlarm: could not create timer fd: %m");
+      return FALSE;
+    }
+
+  memset (&timer_spec, 0, sizeof (timer_spec));
+  timer_spec.it_value.tv_sec = g_date_time_to_unix (self->priv->time) + 1;
+
+  result = timerfd_settime (fd,
+                            TFD_TIMER_ABSTIME | TFD_TIMER_CANCEL_ON_SET,
+                            &timer_spec, NULL);
+
+  if (result < 0)
+    {
+      goa_debug ("GoaAlarm: could not set timer: %m");
+      return FALSE;
+    }
+
+  self->priv->type = GOA_ALARM_TYPE_TIMER;
+  self->priv->timer.stream = g_unix_input_stream_new (fd, TRUE);
+
+  source =
+    g_pollable_input_stream_create_source (G_POLLABLE_INPUT_STREAM
+                                           (self->priv->timer.stream),
+                                           self->priv->cancellable);
+  self->priv->timer.source = source;
+  g_source_set_callback (self->priv->timer.source,
+                         (GSourceFunc) on_timer_source_ready, self,
+                         (GDestroyNotify) clear_timer_source_pointer);
+  g_source_attach (self->priv->timer.source, self->priv->context);
+  g_source_unref (source);
+
+  return TRUE;
+
+#endif /*HAVE_TIMERFD */
+
+  return FALSE;
+}
+
+static gboolean
+on_timeout_source_ready (GoaAlarm *self)
+{
+  g_return_val_if_fail (GOA_IS_ALARM (self), FALSE);
+
+  g_rec_mutex_lock (&self->priv->lock);
+
+  if (g_cancellable_is_cancelled (self->priv->cancellable) ||
+      self->priv->type == GOA_ALARM_TYPE_UNSCHEDULED)
+    goto out;
+
+  fire_or_rearm_alarm (self);
+
+  if (g_cancellable_is_cancelled (self->priv->cancellable))
+    goto out;
+
+  schedule_wakeups_with_timeout_source (self);
+
+out:
+  g_rec_mutex_unlock (&self->priv->lock);
+  return FALSE;
+}
+
+static void
+clear_timeout_source_pointer (GoaAlarm *self)
+{
+  self->priv->timeout.source = NULL;
+}
+
+static void
+schedule_wakeups_with_timeout_source (GoaAlarm *self)
+{
+  GDateTime *now;
+  GSource   *source;
+  GTimeSpan  time_span;
+  guint      interval;
+
+  self->priv->type = GOA_ALARM_TYPE_TIMEOUT;
+
+  now = g_date_time_new_now_local ();
+  time_span = g_date_time_difference (self->priv->time, now);
+  g_date_time_unref (now);
+
+  time_span =
+    CLAMP (time_span, 1000 *G_TIME_SPAN_MILLISECOND,
+           G_MAXUINT *G_TIME_SPAN_MILLISECOND);
+  interval = (guint) time_span / G_TIME_SPAN_MILLISECOND;
+
+  /* We poll every 10 seconds or so because we want to catch time skew
+   */
+  interval = MIN (interval, MAX_TIMEOUT_INTERVAL);
+
+  source = g_timeout_source_new (interval);
+
+  self->priv->timeout.source = source;
+  g_source_set_callback (self->priv->timeout.source,
+                         (GSourceFunc)
+                         on_timeout_source_ready,
+                         self, (GDestroyNotify) clear_timeout_source_pointer);
+
+  g_source_attach (self->priv->timeout.source, self->priv->context);
+  g_source_unref (source);
+}
+
+static void
+schedule_wakeups (GoaAlarm *self)
+{
+  gboolean wakeup_scheduled;
+
+  wakeup_scheduled = schedule_wakeups_with_timerfd (self);
+
+  if (!wakeup_scheduled)
+    {
+      static gboolean seen_before = FALSE;
+
+      if (!seen_before)
+        {
+          goa_debug ("GoaAlarm: falling back to polling timeout");
+          seen_before = TRUE;
+        }
+      schedule_wakeups_with_timeout_source (self);
+    }
+}
+
+static void
+clear_immediate_wakeup_source_pointer (GoaAlarm *self)
+{
+  self->priv->immediate_wakeup_source = NULL;
+}
+
+static void
+schedule_immediate_wakeup (GoaAlarm *self)
+{
+  GSource *source;
+
+  source = g_idle_source_new ();
+
+  self->priv->immediate_wakeup_source = source;
+  g_source_set_callback (self->priv->immediate_wakeup_source,
+                         (GSourceFunc)
+                         on_immediate_wakeup_source_ready,
+                         self,
+                         (GDestroyNotify) clear_immediate_wakeup_source_pointer);
+
+  g_source_attach (self->priv->immediate_wakeup_source, self->priv->context);
+  g_source_unref (source);
+}
+
+void
+goa_alarm_set_time (GoaAlarm *self, GDateTime *time, GCancellable *cancellable)
+{
+  if (g_cancellable_is_cancelled (cancellable))
+    return;
+
+  if (self->priv->cancellable != NULL && self->priv->cancellable != cancellable)
+    g_cancellable_cancel (self->priv->cancellable);
+
+  if (cancellable != NULL)
+    g_object_ref (cancellable);
+
+  if (self->priv->cancelled_id != 0)
+    g_cancellable_disconnect (self->priv->cancellable, self->priv->cancelled_id);
+
+  g_clear_object (&self->priv->cancellable);
+
+  if (cancellable != NULL)
+    self->priv->cancellable = cancellable;
+  else
+    self->priv->cancellable = g_cancellable_new ();
+
+  self->priv->cancelled_id = g_cancellable_connect (self->priv->cancellable,
+                                                    G_CALLBACK (on_cancelled),
+                                                    self, NULL);
+
+  g_date_time_ref (time);
+
+  if (self->priv->time != NULL)
+    g_date_time_unref (self->priv->time);
+
+  self->priv->time = time;
+
+  self->priv->context = g_main_context_ref (g_main_context_default ());
+
+  g_object_notify (G_OBJECT (self), "time");
+
+  schedule_wakeups (self);
+
+  /* Wake up right away, in case it's already expired leaving the gate */
+  schedule_immediate_wakeup (self);
+}
+
+GDateTime *
+goa_alarm_get_time (GoaAlarm *self)
+{
+  return self->priv->time;
+}
+
+GoaAlarm *
+goa_alarm_new (void)
+{
+  GoaAlarm *self;
+
+  self = GOA_ALARM (g_object_new (GOA_TYPE_ALARM, NULL));
+
+  return GOA_ALARM (self);
+}

configure.ac
@@ -204,6 +204,87 @@ if test "$enable_windows_live" != "no"; then
   AC_DEFINE(GOA_WINDOWS_LIVE_ENABLED, 1, [Enable Windows Live data provider])
 fi
 
+# Kerberos
+AC_ARG_ENABLE([kerberos],
+              [AS_HELP_STRING([--enable-kerberos],
+              [Enable kerberos support])],
+              [with_kerberos=$enableval],
+              [enable_kerberos=no])
+AC_PATH_PROG([KRB5_CONFIG], krb5-config, none, $PATH:/usr/kerberos/bin)
+
+if test "x$KRB5_CONFIG" != "xnone"; then
+    KRB5_LIBS="`${KRB5_CONFIG} --libs krb5`"
+    KRB5_CFLAGS="`${KRB5_CONFIG} --cflags krb5`"
+    have_kerberos=yes
+else
+    KRB5_LIBS=""
+    KRB5_CFLAGS=""
+    have_kerberos=no
+fi
+AC_SUBST(KRB5_CFLAGS)
+AC_SUBST(KRB5_LIBS)
+
+if test "$enable_kerberos" != "no"; then
+  if test "$have_kerberos" = "no" ; then
+      AC_MSG_ERROR([kerberos support requested, but not available])
+  fi
+
+  PKG_CHECK_MODULES(GCR, gcr-3)
+  AC_DEFINE(GCR_API_SUBJECT_TO_CHANGE, 1, [Define to use the GCR API])
+fi
+AM_CONDITIONAL(BUILD_KERBEROS, [test x$enable_kerberos != xno])
+
+# Optional timerfd support
+AC_MSG_CHECKING([for timerfd support])
+AC_COMPILE_IFELSE([AC_LANG_PROGRAM([
+#include <sys/timerfd.h>
+#include <unistd.h>
+],[
+int
+main (void)
+{
+  struct itimerspec timer_spec = { 0 };
+  timerfd_settime (timerfd_create (CLOCK_MONOTONIC, TFD_CLOEXEC),
+                   TFD_TIMER_ABSTIME,
+                   &timer_spec,
+                   NULL);
+
+  return 0;
+}
+])],
+[have_timerfd=yes],
+[have_timerfd=no])
+AC_MSG_RESULT($have_timerfd)
+if test x"$have_timerfd" = x"yes"; then
+    AC_DEFINE(HAVE_TIMERFD, 1, [have timerfd support])
+
+    dnl libc headers tend to trail kernel support
+    dnl so compensate if necessary
+    AC_MSG_CHECKING([for timerfd cancel-on-set support])
+    AC_COMPILE_IFELSE([AC_LANG_PROGRAM([
+    #include <sys/timerfd.h>
+    #include <unistd.h>
+    ],[
+    int
+    main (void)
+    {
+      struct itimerspec timer_spec = { 0 };
+      timerfd_settime (timerfd_create (CLOCK_MONOTONIC, TFD_CLOEXEC),
+                       TFD_TIMER_ABSTIME | TFD_TIMER_CANCEL_ON_SET,
+                       &timer_spec,
+                       NULL);
+
+      return 0;
+    }
+    ])],
+    [have_tfd_timer_cancel_on_set=yes],
+    [have_tfd_timer_cancel_on_set=no])
+    AC_MSG_RESULT($have_tfd_timer_cancel_on_set)
+    if test x"$have_tfd_timer_cancel_on_set" = x"no"; then
+       AC_DEFINE(TFD_TIMER_CANCEL_ON_SET, [(1 << 1)], [have timerfd support])
+    fi
+fi
+
 # Internationalization
 #
 
@@ -231,6 +312,7 @@ src/goa/Makefile
 src/goa/goa-1.0.pc
 src/goabackend/Makefile
 src/goabackend/goa-backend-1.0.pc
+src/goaidentity/Makefile
 src/daemon/Makefile
 src/examples/Makefile
 po/Makefile.in

src/Makefile.am
@@ -1,6 +1,6 @@
 
 NULL =
 
-SUBDIRS = goa goabackend daemon examples
+SUBDIRS = goa goaidentity goabackend daemon examples
 
 -include $(top_srcdir)/git.mk

src/daemon/Makefile.am
@@ -1,8 +1,9 @@
-
 NULL =
 
 INCLUDES = 							\
 	-I$(top_builddir)/src -I$(top_srcdir)/src		\
+	-I$(top_builddir)/goaidentity				\
+	-I$(top_srcdir)/goaidentity				\
 	-DPACKAGE_LIBEXEC_DIR=\""$(libexecdir)"\" 		\
 	-DPACKAGE_SYSCONF_DIR=\""$(sysconfdir)"\" 		\
 	-DPACKAGE_DATA_DIR=\""$(datadir)"\" 			\
@@ -15,6 +16,9 @@ INCLUDES = 							\
 	-DGOA_BACKEND_API_IS_SUBJECT_TO_CHANGE			\
 	$(WARN_CFLAGS)						\
 	$(NULL)
+
+BUILT_SOURCES = $(NULL)
+EXTRA_DIST = $(NULL)
 
 libexec_PROGRAMS = goa-daemon
 
@@ -45,6 +49,14 @@ goa_daemon_LDADD = 						\
 	$(LIBNOTIFY_LIBS)					\
 	$(REST_LIBS)						\
 	$(NULL)
+
+if BUILD_KERBEROS
+goa_daemon_LDADD += 						\
+	$(top_builddir)/src/goaidentity/libgoaidentity.la	\
+	$(KRB5_LIBS)						\
+	$(GCR_LIBS)						\
+	$(NULL)
+endif
 
 clean-local :
 	rm -f *~

src/daemon/goadaemon.c
@@ -30,6 +30,7 @@
 #include "goadaemon.h"
 #include "goabackend/goabackend.h"
 #include "goabackend/goautils.h"
+#include "goaidentity/goaidentityservice.h"
 
 struct _GoaDaemon
 {
@@ -45,6 +46,7 @@ struct _GoaDaemon
 
   GoaManager *manager;
 
+  GoaIdentityService *identity_service;
   NotifyNotification *notification;
 
   guint config_timeout_id;
@@ -126,6 +128,8 @@ goa_daemon_finalize (GObject *object)
   g_object_unref (daemon->object_manager);
   g_object_unref (daemon->connection);
 
+  g_clear_object (&daemon->identity_service);
+
   G_OBJECT_CLASS (goa_daemon_parent_class)->finalize (object);
 }
 
@@ -189,6 +193,7 @@ goa_daemon_init (GoaDaemon *daemon)
   static volatile GQuark goa_error_domain = 0;
   GoaObjectSkeleton *object;
   gchar *path;
+  GError *error = NULL;
 
   /* this will force associating errors in the GOA_ERROR error domain
    * with org.freedesktop.Goa.Error.* errors via g_dbus_error_register_error_domain().
@@ -235,6 +240,15 @@ goa_daemon_init (GoaDaemon *daemon)
 
   /* Export objects */
   g_dbus_object_manager_server_set_connection (daemon->object_manager, daemon->connection);
+
+  daemon->identity_service = goa_identity_service_new ();
+  if (!goa_identity_service_activate (daemon->identity_service,
+                                      &error))
+    {
+      goa_warning ("Error activating identity service: %s", error->message);
+      g_error_free (error);
+      g_clear_object (&daemon->identity_service);
+    }
 }
 
 static void

src/goaidentity/Makefile.am
@@ -1,0 +1,136 @@
+NULL =
+BUILT_SOURCES = $(NULL)
+EXTRA_DIST = $(NULL)
+CLEANFILES= $(NULL)
+
+INCLUDES = 							\
+	-I$(top_builddir)/src -I$(top_srcdir)/src		\
+	-I$(top_srcdir)/src/goabackend				\
+	-DPACKAGE_LIBEXEC_DIR=\""$(libexecdir)"\" 		\
+	-DPACKAGE_SYSCONF_DIR=\""$(sysconfdir)"\" 		\
+	-DPACKAGE_DATA_DIR=\""$(datadir)"\" 			\
+	-DPACKAGE_BIN_DIR=\""$(bindir)"\" 			\
+	-DPACKAGE_LOCALSTATE_DIR=\""$(localstatedir)"\" 	\
+	-DPACKAGE_LOCALE_DIR=\""$(localedir)"\" 		\
+	-DPACKAGE_LIB_DIR=\""$(libdir)"\" 			\
+	-D_POSIX_PTHREAD_SEMANTICS -D_REENTRANT			\
+	-DGOA_API_IS_SUBJECT_TO_CHANGE				\
+	-DGOA_BACKEND_API_IS_SUBJECT_TO_CHANGE			\
+	$(WARN_CFLAGS)						\
+	$(NULL)
+
+identity_headers =						\
+	goaalarm.h						\
+	goaidentity.h						\
+	goaidentityinquiry.h					\
+	goaidentityinquiryprivate.h				\
+	goaidentityservice.h					\
+	goaidentitymanagerprivate.h				\
+	goaidentitymanager.h					\
+	goaidentityutils.h					\
+	goakerberosidentity.h					\
+	goakerberosidentityinquiry.h				\
+	goakerberosidentitymanager.h				\
+	um-realm-manager.h					\
+	$(NULL)
+
+identity_sources =						\
+	$(identity_headers)					\
+	goaalarm.c						\
+	goaidentity.c						\
+	goaidentityinquiry.c					\
+	goaidentityservice.c					\
+	goaidentitymanager.c					\
+	goaidentityutils.c					\
+	goakerberosidentity.c					\
+	goakerberosidentityinquiry.c				\
+	goakerberosidentitymanager.c				\
+	um-realm-manager.c					\
+	$(NULL)
+
+identity_dbus_built_sources =					\
+	org.gnome.Identity.c		org.gnome.Identity.h	\
+	$(NULL)
+
+$(identity_dbus_built_sources) : Makefile.am org.gnome.Identity.xml
+	gdbus-codegen						\
+		--interface-prefix org.gnome.Identity.		\
+		--c-namespace GoaIdentityService		\
+		--c-generate-object-manager			\
+		--generate-c-code org.gnome.Identity		\
+		org.gnome.Identity.xml				\
+		--annotate "org.gnome.Identity"			\
+		           "org.gtk.GDBus.C.Name" Identity	\
+		$(NULL)
+BUILT_SOURCES += $(identity_dbus_built_sources)
+EXTRA_DIST += org.gnome.Identity.xml
+
+goaidentityenumtypes.h: goaidentityenumtypes.h.in $(identity_headers)
+	$(AM_V_GEN) glib-mkenums --template $^ > $@
+EXTRA_DIST += goaidentityenumtypes.h.in
+BUILT_SOURCES += goaidentityenumtypes.h
+
+goaidentityenumtypes.c: goaidentityenumtypes.c.in $(identity_headers)
+	$(AM_V_GEN) glib-mkenums --template $^ > $@
+EXTRA_DIST += goaidentityenumtypes.c.in
+BUILT_SOURCES += goaidentityenumtypes.c
+
+realmd_dbus_built_sources =					\
+	um-realm-generated.h					\
+	um-realm-generated.c					\
+	$(NULL)
+
+$(realmd_dbus_built_sources) : Makefile.am org.freedesktop.realmd.xml
+	gdbus-codegen						\
+		--interface-prefix org.freedesktop.realmd.	\
+		--generate-c-code um-realm-generated		\
+		--c-generate-object-manager			\
+		--c-namespace UmRealm				\
+		--annotate "org.freedesktop.realmd.Realm"	\
+		            org.gtk.GDBus.C.Name Common		\
+		org.freedesktop.realmd.xml			\
+		$(NULL)
+BUILT_SOURCES += $(realmd_dbus_built_sources)
+EXTRA_DIST += org.freedesktop.realmd.xml
+
+if BUILD_KERBEROS
+noinst_LTLIBRARIES = libgoaidentity.la
+
+libgoaidentity_la_SOURCES = 					\
+	goaidentityenumtypes.h		goaidentityenumtypes.c	\
+	$(identity_dbus_built_sources)				\
+	$(realmd_dbus_built_sources)				\
+	$(identity_sources)					\
+	$(NULL)
+
+libgoaidentity_la_CPPFLAGS =					\
+	-DG_LOG_DOMAIN=\"libgoaidentity\"			\
+	-DGOA_BACKEND_COMPILATION				\
+	$(NULL)
+
+libgoaidentity_la_CFLAGS =					\
+	$(GLIB_CFLAGS) 						\
+	$(GTK_CFLAGS)						\
+	$(LIBNOTIFY_CFLAGS)					\
+	$(KRB5_CFLAGS)						\
+	$(GCR_CFLAGS)						\
+	$(NULL)
+
+libgoaidentity_la_LIBADD = 					\
+	$(GLIB_LIBS) 						\
+	$(GTK_LIBS)						\
+	$(LIBNOTIFY_LIBS)					\
+	$(KRB5_LIBS)						\
+	$(GCR_LIBS)						\
+	$(NULL)
+
+else
+EXTRA_DIST += $(identity_sources)
+endif
+
+CLEANFILES += $(BUILT_SOURCES)
+
+clean-local :
+	rm -f *~
+
+-include $(top_srcdir)/git.mk

src/goaidentity/goaalarm.h
@@ -1,0 +1,64 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2012 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ *
+ * Authors: Ray Strode
+ */
+
+#ifndef __GOA_ALARM_H__
+#define __GOA_ALARM_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+#define GOA_TYPE_ALARM             (goa_alarm_get_type ())
+#define GOA_ALARM(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), GOA_TYPE_ALARM, GoaAlarm))
+#define GOA_ALARM_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), GOA_TYPE_ALARM, GoaAlarmClass))
+#define GOA_IS_ALARM(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GOA_TYPE_ALARM))
+#define GOA_IS_ALARM_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), GOA_TYPE_ALARM))
+#define GOA_ALARM_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS((obj), GOA_TYPE_ALARM, GoaAlarmClass))
+typedef struct _GoaAlarm GoaAlarm;
+typedef struct _GoaAlarmClass GoaAlarmClass;
+typedef struct _GoaAlarmPrivate GoaAlarmPrivate;
+
+struct _GoaAlarm
+{
+  GObject parent;
+
+  GoaAlarmPrivate *priv;
+};
+
+struct _GoaAlarmClass
+{
+  GObjectClass parent_class;
+
+  void (* fired)   (GoaAlarm *alarm);
+  void (* rearmed) (GoaAlarm *alarm);
+};
+
+GType goa_alarm_get_type (void);
+
+GoaAlarm *goa_alarm_new (void);
+void goa_alarm_set_time (GoaAlarm     *alarm,
+                         GDateTime    *time,
+                         GCancellable *cancellable);
+GDateTime *goa_alarm_get_time (GoaAlarm *alarm);
+G_END_DECLS
+#endif /* __GOA_ALARM_H__ */

commit bcec5f148abbab1740e0f5fafa174ae9fa2bf0da

alarm: Remove the unused default handler vfuncs
These default handler vfuncs aren't actually associated with their
respective signals because they aren't specified in g_signal_new. Hence
they are completely useless at the moment.

This is a final class, so there isn't going to be any sub-class that
would want to override the default signal handlers. GoaAlarm might
still need a default handler for itself, but the same effect can be
achieved with a simple private function. It is a small price to pay for
being able to use G_DECLARE_FINAL_TYPE.

Fallout from a43777eb40efb6dae5128397faa8000e59ba0f04

https://gitlab.gnome.org/GNOME/gnome-online-accounts/merge_requests/40

src/goaidentity/goaalarm.c
@@ -59,9 +59,6 @@ struct _GoaAlarm
 struct _GoaAlarmClass
 {
   GObjectClass parent_class;
-
-  void (* fired)   (GoaAlarm *alarm);
-  void (* rearmed) (GoaAlarm *alarm);
 };
 
 enum