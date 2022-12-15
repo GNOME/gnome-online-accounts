dc59c8f4de0de1dc86c45613267cf2ee353a6d6a
Ray Strode <rstrode@redhat.com>
2022-12-15 14:46:01 -0500

    kerberos-identity: Fix buglet in update_identity
    The update_identity function is supposed to transfer the identity
    form one object to another.
    
    In practice, this is currently always a noop because only objects
    with the same identities get copied to each other.
    
    Nevertheless, there is a bug in the function. It grabs the identity
    from the target object instead of from the source object.
    
    This commit fixes that.
    
    
    (cherry picked from commit 8c5b63069dfbf36ac662369d9ba3fb4ab0d2efb2)

--- src/goaidentity/goakerberosidentity.c
+++ src/goaidentity/goakerberosidentity.c
@@ -1507,7 +1507,7 @@ update_identifier (GoaKerberosIdentity *self, GoaKerberosIdentity *new_identity)
 {
   char *new_identifier;
 
-  new_identifier = get_identifier (self, NULL);
+  new_identifier = get_identifier (new_identity, NULL);
   if (g_strcmp0 (self->identifier, new_identifier) != 0 && new_identifier != NULL)
     {
       g_free (self->identifier);
