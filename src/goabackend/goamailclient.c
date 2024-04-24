9a879239c35fabec3f81633694eab5acae331929
Andy Holmes <andyholmes@gnome.org>
2024-04-24 09:39:42 -0700

    goamailclient: add support for IMAP/SMTP autoconfig
    Add basic initial support for Autoconfig, reporting IMAP/SMTP
    configurations for a given email address.

--- src/goabackend/goamailclient.c
+++ src/goabackend/goamailclient.c
@@ -19,12 +19,40 @@
 #include "config.h"
 
 #include <glib/gi18n-lib.h>
+#include <rest/rest-xml-parser.h>
 
 #include "goamailclient.h"
+#include "goasouplogger.h"
 #include "goautils.h"
 
 /* The timeout used for non-IDLE commands */
 #define COMMAND_TIMEOUT_SEC 30
+
+/* Mail Autoconfig URIs
+ *
+ * See:
+ *  - Mail provider (https://benbucksch.github.io/autoconfig-spec/draft-autoconfig-1.html#section-4.1)
+ *  - Central database (https://benbucksch.github.io/autoconfig-spec/draft-autoconfig-1.html#section-4.2)
+ *  - MX (https://benbucksch.github.io/autoconfig-spec/draft-autoconfig-1.html#section-4.3)
+ *  - Autoconfig Spec issue #7 (https://github.com/benbucksch/autoconfig-spec/issues/7)
+ */
+#define AUTOCONFIG_DOMAIN_ADDRESS_FMT      "https://autoconfig.%s/.well-known/mail-v1.xml?emailaddress=%s"
+#define AUTOCONFIG_DOMAIN_ADDRESS_OLD_FMT  "https://autoconfig.%s/mail/config-v1.1.xml?emailaddress=%s"
+#define AUTOCONFIG_DOMAIN_FMT              "https://%s/.well-known/autoconfig/mail/config-v1.1.xml"
+#define AUTOCONFIG_DOMAIN_ADDRESS_OLD_FMT  "https://autoconfig.%s/mail/config-v1.1.xml?emailaddress=%s"
+#define AUTOCONFIG_DOMAIN_OLD_FMT          "http://autoconfig.%s/mail/config-v1.1.xml"
+#define AUTOCONFIG_MX_DOMAIN_ADDRESS_FMT   AUTOCONFIG_DOMAIN_ADDRESS_FMT
+#define AUTOCONFIG_DATABASE_ISPDB_FMT      "https://v1.ispdb.net/%s"
+#define AUTOCONFIG_DATABSE_THUNDERBIRD_FMT "https://autoconfig.thunderbird.net/v1.1/%s"
+
+#define AUTOCONFIG_SERVER_TYPE_IMAP "imap"
+#define AUTOCONFIG_SERVER_TYPE_POP3 "pop3"
+#define AUTOCONFIG_SERVER_TYPE_JMAP "jmap"
+#define AUTOCONFIG_SERVER_TYPE_SMTP "smtp"
+
+#define AUTOCONFIG_SOCKET_TYPE_PLAIN    "plain"
+#define AUTOCONFIG_SOCKET_TYPE_STARTTLS "STARTTLS"
+#define AUTOCONFIG_SOCKET_TYPE_SSL      "SSL"
 
 struct _GoaMailClient
 {
@@ -468,3 +496,442 @@ goa_mail_client_check_sync (GoaMailClient  *self,
 
   return data.op_res;
 }
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+static GoaMailConfig *
+goa_mail_config_new_from_xml (RestXmlNode *server,
+                              const char  *email_address,
+                              const char  *email_localpart,
+                              const char  *email_domain)
+{
+  GoaMailConfig *config = NULL;
+  const char *server_type = NULL;
+  GoaTlsType tls_type;
+  RestXmlNode *username = NULL;
+  RestXmlNode *hostname = NULL;
+  RestXmlNode *port = NULL;
+  RestXmlNode *socket_type = NULL;
+
+  g_return_val_if_fail (g_str_equal (server->name, "incomingServer")
+                        || g_str_equal (server->name, "outgoingServer"), NULL);
+
+  /* GOA only supports IMAP/SMTP, so all other server types are ignored.
+   */
+  server_type = rest_xml_node_get_attr (server, "type");
+  if (g_strcmp0 (server_type, AUTOCONFIG_SERVER_TYPE_IMAP) != 0
+      && g_strcmp0 (server_type, AUTOCONFIG_SERVER_TYPE_SMTP) != 0)
+    {
+      g_debug ("%s(): ignoring \"%s\" server", G_STRFUNC, server_type);
+      return NULL;
+    }
+
+  socket_type = rest_xml_node_find (server, "socketType");
+  if (socket_type == NULL || socket_type->content == NULL)
+    return NULL;
+  else if (g_ascii_strcasecmp (socket_type->content, AUTOCONFIG_SOCKET_TYPE_SSL) == 0)
+    tls_type = GOA_TLS_TYPE_SSL;
+  else if (g_ascii_strcasecmp (socket_type->content, AUTOCONFIG_SOCKET_TYPE_STARTTLS) == 0)
+    tls_type = GOA_TLS_TYPE_STARTTLS;
+  else if (g_ascii_strcasecmp (socket_type->content, AUTOCONFIG_SOCKET_TYPE_PLAIN) == 0)
+    tls_type = GOA_TLS_TYPE_NONE;
+  else
+    return NULL;
+
+  hostname = rest_xml_node_find (server, "hostname");
+  g_return_val_if_fail (hostname != NULL && hostname->content != NULL, NULL);
+
+  config = g_new0 (GoaMailConfig, 1);
+  config->server_type = g_strdup (server_type);
+  config->hostname = g_strdup (hostname->content);
+
+  port = rest_xml_node_find (server, "port");
+  if (port != NULL && port->content != NULL)
+    {
+      guint64 port_ = g_ascii_strtoull (port->content, NULL, 10);
+
+      if (port_ > 0 && port_ < UINT16_MAX)
+        config->port = (uint16_t)port_;
+    }
+
+  if (config->port == 0)
+    {
+      if (g_ascii_strcasecmp (server_type, AUTOCONFIG_SERVER_TYPE_IMAP) == 0)
+        config->port = (tls_type == GOA_TLS_TYPE_SSL) ? 993 : 143;
+      else if (g_ascii_strcasecmp (server_type, AUTOCONFIG_SERVER_TYPE_SMTP) == 0)
+        config->port = (tls_type == GOA_TLS_TYPE_SSL) ? 465 : 587;
+    }
+
+  username = rest_xml_node_find (server, "username");
+  if (username != NULL && username->content != NULL)
+    {
+      if (g_ascii_strcasecmp (username->content, "%EMAILADDRESS%") == 0)
+        config->username = g_strdup (email_address);
+      else if (g_ascii_strcasecmp (username->content, "%EMAILLOCALPART%") == 0)
+        config->username = g_strdup (email_localpart);
+      else if (g_ascii_strcasecmp (username->content, "%EMAILDOMAIN%") == 0)
+        config->username = g_strdup (email_domain);
+    }
+
+  return g_steal_pointer (&config);
+}
+
+/*< private >
+ * goa_mail_config_parse_autoconfig_xml:
+ * @xml_bytes: Autoconfig XML
+ * @email_address: a valid email address
+ * @error: (nullable): return location for an error
+ *
+ * Parse an autoconfig XML document and return a list of services.
+ *
+ * See:
+ *  - https://github.com/benbucksch/autoconfig-spec/
+ *  - https://benbucksch.github.io/autoconfig-spec/draft-autoconfig-1.html
+ *  - https://github.com/thunderbird/autoconfig
+ *
+ * Returns: (transfer container) (element-type Goa.MailConfig): a list of discovered services
+ */
+static GPtrArray *
+goa_mail_config_parse_autoconfig_xml (GBytes      *xml_bytes,
+                                      const char  *email_address,
+                                      GError     **error)
+{
+  g_autoptr(GPtrArray) services = NULL;
+  g_autofree char *email_localpart = NULL;
+  g_autofree char *email_domain = NULL;
+  g_autoptr(RestXmlParser) parser = NULL;
+  g_autoptr(RestXmlNode) root_node = NULL;
+  RestXmlNode *email_node = NULL;
+  RestXmlNode *incoming_server = NULL;
+  RestXmlNode *outgoing_server = NULL;
+  const char *xml_data = NULL;
+  gsize xml_len = 0;
+
+  goa_utils_parse_email_address (email_address, &email_localpart, &email_domain);
+  xml_data = g_bytes_get_data (xml_bytes, &xml_len);
+
+  parser = rest_xml_parser_new ();
+  root_node = rest_xml_parser_parse_from_data (parser, xml_data, xml_len);
+  if (root_node == NULL)
+    {
+      g_set_error_literal (error,
+                           G_IO_ERROR,
+                           G_IO_ERROR_INVALID_DATA,
+                           "Failed to parse Autoconfig XML");
+      return NULL;
+    }
+
+  services = g_ptr_array_new_with_free_func ((GDestroyNotify)goa_mail_config_free);
+
+  /* Mail Services
+   */
+  email_node = rest_xml_node_find (root_node, "emailProvider");
+  if (email_node == NULL)
+    {
+      g_set_error_literal (error,
+                           G_IO_ERROR,
+                           G_IO_ERROR_NOT_SUPPORTED,
+                           "Missing \"emailProvider\" in Autoconfig XML");
+      return NULL;
+    }
+
+  incoming_server = rest_xml_node_find (email_node, "incomingServer");
+  for (RestXmlNode *node = incoming_server; node != NULL; node = node->next)
+    {
+      GoaMailConfig *server = NULL;
+
+      server = goa_mail_config_new_from_xml (node, email_address, email_localpart, email_domain);
+      if (server != NULL)
+        g_ptr_array_add (services, g_steal_pointer (&server));
+    }
+
+  outgoing_server = rest_xml_node_find (email_node, "outgoingServer");
+  for (RestXmlNode *node = outgoing_server; node != NULL; node = node->next)
+    {
+      GoaMailConfig *server = NULL;
+
+      server = goa_mail_config_new_from_xml (node, email_address, email_localpart, email_domain);
+      if (server != NULL)
+        g_ptr_array_add (services, g_steal_pointer (&server));
+    }
+
+  return g_steal_pointer (&services);
+}
+
+/* ---------------------------------------------------------------------------------------------------- */
+
+static int
+sort_mx_domains (gconstpointer a,
+                 gconstpointer b)
+{
+  GVariant *variant_a = (GVariant *)a;
+  GVariant *variant_b = (GVariant *)b;
+  uint16_t pref_a, pref_b;
+
+  g_variant_get (variant_a, "(qs)", &pref_a, NULL);
+  g_variant_get (variant_b, "(qs)", &pref_b, NULL);
+
+  return pref_a > pref_b;
+}
+
+typedef struct
+{
+  SoupSession *session;
+  char *email_address;
+  GQueue uris;
+  gboolean mx_fallback;
+} DiscoverData;
+
+static void
+discover_data_free (gpointer user_data)
+{
+  DiscoverData *data = user_data;
+
+  g_clear_object (&data->session);
+  g_clear_pointer (&data->email_address, g_free);
+  g_queue_clear_full (&data->uris, g_free);
+  g_free (data);
+}
+
+static void mail_client_discover_iterate (GTask *task);
+
+static void
+goa_mail_client_autoconfig_cb (SoupSession  *session,
+                               GAsyncResult *result,
+                               gpointer      user_data)
+{
+  g_autoptr (GTask) task = G_TASK (user_data);
+  DiscoverData *data = g_task_get_task_data (task);
+  SoupMessage *response = NULL;
+  GPtrArray *services = NULL;
+  g_autoptr (GBytes) body = NULL;
+  g_autoptr (GError) error = NULL;
+
+  body = soup_session_send_and_read_finish (session, result, &error);
+  if (error != NULL)
+    {
+      if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+        {
+          g_task_return_error (task, g_steal_pointer (&error));
+          return;
+        }
+
+      goto next;
+    }
+
+  response = soup_session_get_async_result_message (session, result);
+  if (soup_message_get_status (response) != SOUP_STATUS_OK)
+    goto next;
+
+  services = goa_mail_config_parse_autoconfig_xml (body, data->email_address, NULL);
+  if (services != NULL)
+    {
+      g_task_return_pointer (task, g_steal_pointer (&services), (GDestroyNotify) g_ptr_array_unref);
+      return;
+    }
+
+next:
+  mail_client_discover_iterate (task);
+}
+
+static void
+goa_mail_client_mx_domain_cb (GResolver    *resolver,
+                              GAsyncResult *result,
+                              gpointer      user_data)
+{
+  g_autoptr(GTask) task = G_TASK (user_data);
+  DiscoverData *data = g_task_get_task_data (task);
+  const char *mx_full_domain = NULL;
+  const char *mx_base_domain = NULL;
+  g_autolist(GVariant) records = NULL;
+  g_autoptr(GError) error = NULL;
+
+  /* Resolve the highest priority MX domain for %EMAILDOMAIN% */
+  records = g_resolver_lookup_records_finish (resolver, result, &error);
+  if (records == NULL)
+    {
+      if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+        {
+          g_task_return_error (task, g_steal_pointer (&error));
+          return;
+        }
+
+      g_debug ("%s(): %s", G_STRFUNC, error->message);
+      goto out;
+    }
+
+  records = g_list_sort (records, sort_mx_domains);
+  g_variant_get ((GVariant *)records->data, "(q&s)", NULL, &mx_full_domain);
+  mx_base_domain = soup_tld_get_base_domain (mx_full_domain, NULL);
+
+  /* Lookup by the highest priority %MXFULLDOMAIN% and %MXBASEDOMAIN */
+  g_queue_push_tail (&data->uris, g_strdup_printf (AUTOCONFIG_MX_DOMAIN_ADDRESS_FMT, mx_full_domain, data->email_address));
+  if (mx_base_domain != NULL && g_strcmp0 (mx_base_domain, mx_full_domain) != 0)
+    g_queue_push_tail (&data->uris, g_strdup_printf (AUTOCONFIG_MX_DOMAIN_ADDRESS_FMT, mx_base_domain, data->email_address));
+
+  g_queue_push_tail (&data->uris, g_strdup_printf (AUTOCONFIG_DATABASE_ISPDB_FMT, mx_full_domain));
+  if (mx_base_domain != NULL && g_strcmp0 (mx_base_domain, mx_full_domain) != 0)
+    g_queue_push_tail (&data->uris, g_strdup_printf (AUTOCONFIG_DATABASE_ISPDB_FMT, mx_base_domain));
+
+out:
+  mail_client_discover_iterate (task);
+}
+
+static void
+mail_client_discover_iterate (GTask *task)
+{
+  DiscoverData *data = g_task_get_task_data (task);
+
+  if (!g_queue_is_empty (&data->uris))
+    {
+      g_autoptr(SoupMessage) message = NULL;
+      g_autofree char *uri = NULL;
+
+      uri = g_queue_pop_head (&data->uris);
+      message = soup_message_new (SOUP_METHOD_GET, uri);
+
+      if (message != NULL)
+        {
+          soup_session_send_and_read_async (data->session,
+                                            message,
+                                            G_PRIORITY_DEFAULT,
+                                            g_task_get_cancellable (task),
+                                            (GAsyncReadyCallback)goa_mail_client_autoconfig_cb,
+                                            g_object_ref (task));
+        }
+      else
+        {
+          g_warning ("Failed to create message for \"%s\"", uri);
+          mail_client_discover_iterate (task);
+        }
+    }
+  else if (data->mx_fallback)
+    {
+      g_autoptr(GResolver) resolver = NULL;
+      g_autofree char *email_domain = NULL;
+
+      goa_utils_parse_email_address (data->email_address, NULL, &email_domain);
+      resolver = g_resolver_get_default ();
+      g_resolver_lookup_records_async (resolver,
+                                       email_domain,
+                                       G_RESOLVER_RECORD_MX,
+                                       g_task_get_cancellable (task),
+                                       (GAsyncReadyCallback)goa_mail_client_mx_domain_cb,
+                                       g_object_ref (task));
+      data->mx_fallback = FALSE;
+    }
+  else
+    {
+      g_task_return_new_error (task,
+                               GOA_ERROR,
+                               GOA_ERROR_FAILED,
+                               "Failed to autoconfig '%s'",
+                               data->email_address);
+    }
+}
+
+/**
+ * goa_mail_client_discover:
+ * @self: a `GoaMailClient`
+ * @email_address: a well-formed email address
+ * @cancellable: (nullable): a cancellable for the operation
+ * @callback: (scope async): a callback to be invoked when the operation is complete
+ * @user_data: (closure): user supplied data
+ *
+ * Discover IMAP/SMTP configuration for the domain of @email.
+ *
+ * Call [method@Goa.MailClient.discover_finish] to get the result.
+ */
+void
+goa_mail_client_discover (GoaMailClient       *self,
+                          const char          *email_address,
+                          GCancellable        *cancellable,
+                          GAsyncReadyCallback  callback,
+                          gpointer             user_data)
+{
+  g_autoptr(GTask) task = NULL;
+  g_autoptr(SoupLogger) logger = NULL;
+  g_autofree char *email_domain = NULL;
+  DiscoverData *data;
+
+  g_return_if_fail (GOA_IS_MAIL_CLIENT (self));
+  g_return_if_fail (email_address != NULL && *email_address != '\0');
+  g_return_if_fail (cancellable == NULL || G_IS_CANCELLABLE (cancellable));
+
+  if (!goa_utils_parse_email_address (email_address, NULL, &email_domain))
+    {
+      g_task_report_new_error (self, callback, user_data, goa_mail_client_discover,
+                               G_IO_ERROR,
+                               G_IO_ERROR_INVALID_ARGUMENT,
+                               _("Invalid email address “%s”"),
+                               email_address);
+    }
+
+  data = g_new0 (DiscoverData, 1);
+  data->email_address = g_strdup (email_address);
+  data->mx_fallback = TRUE;
+
+  data->session = soup_session_new ();
+  logger = goa_soup_logger_new (SOUP_LOGGER_LOG_BODY, -1);
+  soup_session_add_feature (data->session, SOUP_SESSION_FEATURE (logger));
+  soup_session_set_timeout (data->session, 15);
+  soup_session_set_user_agent (data->session, "gnome-online-accounts/" PACKAGE_VERSION " ");
+
+  /* Lookup by %EMAILDOMAIN% */
+  g_queue_init (&data->uris);
+  g_queue_push_tail (&data->uris, g_strdup_printf (AUTOCONFIG_DOMAIN_ADDRESS_FMT, email_domain, email_address));
+  g_queue_push_tail (&data->uris, g_strdup_printf (AUTOCONFIG_DOMAIN_ADDRESS_OLD_FMT, email_domain, email_address));
+  g_queue_push_tail (&data->uris, g_strdup_printf (AUTOCONFIG_DOMAIN_FMT, email_domain));
+  g_queue_push_tail (&data->uris, g_strdup_printf (AUTOCONFIG_DOMAIN_OLD_FMT, email_domain));
+  g_queue_push_tail (&data->uris, g_strdup_printf (AUTOCONFIG_DATABASE_ISPDB_FMT, email_domain));
+
+  task = g_task_new (self, cancellable, callback, user_data);
+  g_task_set_source_tag (task, goa_mail_client_discover);
+  g_task_set_task_data (task, data, discover_data_free);
+
+  mail_client_discover_iterate (task);
+}
+
+/**
+ * goa_mail_client_discover_finish:
+ * @self: a `GoaMailClient`
+ * @result: a `GAsyncResult`
+ * @error: (nullable): return location for an error
+ *
+ * Finish an operation started by [method@Goa.MailClient.discover].
+ *
+ * Returns: (transfer container) (element-type Goa.MailConfig): a list of discovered services,
+ *   which may include DAV services, or %NULL with @error set
+ */
+GPtrArray *
+goa_mail_client_discover_finish (GoaMailClient  *self,
+                                 GAsyncResult   *result,
+                                 GError        **error)
+{
+  GTask *task = G_TASK (result);
+
+  g_return_val_if_fail (GOA_IS_MAIL_CLIENT (self), NULL);
+  g_return_val_if_fail (g_task_is_valid (result, self), NULL);
+  g_return_val_if_fail (g_task_get_source_tag (task) == goa_mail_client_discover, NULL);
+  g_return_val_if_fail (error == NULL || *error == NULL, NULL);
+
+  return g_task_propagate_pointer (task, error);
+}
+
+/**
+ * goa_mail_config_free:
+ * @config: a `GoaMailConfig`
+ *
+ * Free a mail server configuration.
+ */
+void
+goa_mail_config_free (GoaMailConfig *config)
+{
+  g_return_if_fail (config != NULL);
+
+  g_clear_pointer (&config->server_type, g_free);
+  g_clear_pointer (&config->hostname, g_free);
+  g_clear_pointer (&config->username, g_free);
+  g_free (config);
+}
+
