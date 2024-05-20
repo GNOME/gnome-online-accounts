800227f098d74e94bee3d43abe7e656b842e0ce7
Andy Holmes <andyholmes@gnome.org>
2024-05-20 12:54:52 -0700

    goabackend: add a generic collection provider
    Add a provider which can create accounts covering IMAP/SMTP and WebDAV,
    emulating Evolution's "collection" account.
    
    This account type is intended to fill the backend role of accounts
    that result from auto-configuration and discovery.

---
+++ src/goabackend/goacollectionprovider.h
@@ -0,0 +1,38 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/*
+ * Copyright © 2023 GNOME Foundation Inc.
+ * Contributor: Andy Holmes <andyholmes@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined (__GOA_BACKEND_INSIDE_GOA_BACKEND_H__) && !defined (GOA_BACKEND_COMPILATION)
+#error "Only <goabackend/goabackend.h> can be included directly."
+#endif
+
+#ifndef __GOA_COLLECTION_PROVIDER_H__
+#define __GOA_COLLECTION_PROVIDER_H__
+
+#include <glib-object.h>
+
+#include "goaprovider-priv.h"
+
+G_BEGIN_DECLS
+
+#define GOA_TYPE_COLLECTION_PROVIDER (goa_collection_provider_get_type ())
+G_DECLARE_DERIVABLE_TYPE (GoaCollectionProvider, goa_collection_provider, GOA, COLLECTION_PROVIDER, GoaProvider);
+
+G_END_DECLS
+
+#endif /* __GOA_COLLECTION_PROVIDER_H__ */
